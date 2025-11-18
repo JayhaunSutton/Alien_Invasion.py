dd49e10f0589c46a02a0c74eae2417fa3eb43790
JayhaunSutton <lazyjayhaun1234@gmail.com>
2025-11-18 00:27:22 -0500

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,11 @@
+class Settings:
+    "A class to store the games settings"
+
+    def __init__(self):
+        """initialize the game settings"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 720
+        self.bg_color = (230, 230, 230)
+        #Ship settings
+        self.ship_speed = 1.5
